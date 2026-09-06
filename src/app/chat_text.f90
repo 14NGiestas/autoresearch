ff4e9658ec176f81671f0812de823e3e128a9818
Ian Giestas Pauli <iangiestas@usp.br>
2026-09-05 23:29:25 -0300

    chat: customizable templating + streaming + rsp
    - chat_text/repl: --template (default '### USER\n{prompt}\n\n### ASSISTANT\n', 'raw' for completion), --system, --stop (comma-separated, truncates), --stream (flush per token). Template supports {prompt} + \n escapes.
    - M_CLI2 response files: 'use flake' fixed, chat.rsp added (tables/weights/n/temp/pres/freq/nblock). Use: chat_text @chat.rsp --n 60 < prompt.txt, repl @chat.rsp --stream
    - Verified: raw -> completion, default -> chat (Hello -> 'The user wants...'), --system / --stream work, ldd OK after nixpkgs bump.

--- src/app/chat_text.f90
+++ src/app/chat_text.f90
@@ -18,7 +18,7 @@ program chat_text
   use tokenizer_encode_mod
   use sample_mod, only: sample_token, sample_next
   use fortran_kv_mod, only: gpt_step
-  use M_CLI2, only: set_args, sget, rget, iget, specified
+  use M_CLI2, only: set_args, set_mode, sget, rget, iget, specified
   implicit none
 
   integer, parameter :: sp = c_float
@@ -27,6 +27,7 @@ program chat_text
 
   character(len=512) :: tdir, wdir, arg
   character(len=1) :: cb
+  character(len=4096) :: tmpl_raw, sys_raw, stop_raw
   integer :: n_gen, u, ios, fsize, i, j, best, tc, step, nprompt
   integer :: ntot, d2, clen
   real(sp), allocatable :: ckv(:), cvv(:), out1(:)
@@ -34,10 +35,11 @@ program chat_text
   real(sp) :: temp = 0.0_sp, topp = 1.0_sp, pres = 0.0_sp, freq = 0.0_sp
   integer :: nblock = 0
   logical :: dostats = .false.
+  logical :: dostream = .false.
   integer :: s0, s1, srate
   integer(c_int64_t) :: cms_pre = 0, cms_dec = 0
-  integer, allocatable :: pbytes(:), pids(:), idx(:), obytes(:)
-  integer :: nbytes
+  integer, allocatable :: pbytes(:), pids(:), idx(:), obytes(:), sbytes(:)
+  integer :: nbytes, snbytes
   real(sp), allocatable :: cos_b(:), sin_b(:)
   real(sp), allocatable :: wte(:), lm(:)
   real(sp), allocatable :: c_q(:), c_k(:), c_v(:), c_pr(:), c_fc(:), c_pr2(:)
@@ -45,14 +47,16 @@ program chat_text
   real(sp) :: theta, ang, mchk
   d2 = HD / 2
 
+  call set_mode('response_file')
   call set_args('--tables TABLES --weights WEIGHTS --n 20 --temp 0.0' // &
       ' --seed 12345 --topp 1.0 --pres 0.0 --freq 0.0 --nblock 0' // &
-      ' --stats F', &
+      ' --stats F --template T --system S --stop S --stream F', &
       help_text=[character(len=80) :: &
       'NAME', &
       '  chat_text - pure-Fortran text-in/text-out GPT inference', &
       'SYNOPSIS', &
       '  chat_text --tables DIR --weights DIR --n N [options] < stdin', &
+      '  chat_text @rsp_file (response file, see chat.rsp)', &
       'OPTIONS', &
       '  --tables DIR  tokenizer tables (ranks.txt, unicode_*.txt)', &
       '  --weights DIR checkpoint .npy set (see export_weights.py)', &
@@ -63,8 +67,13 @@ program chat_text
       '  --pres X      presence penalty', &
       '  --freq X      frequency penalty', &
       '  --nblock N    no-repeat n-gram size (0 = off)', &
+      '  --template S  chat template with {prompt} (default: chat)', &
+      '                use "raw" for no wrapping (completion mode)', &
+      '  --system S    system prompt (prepended as ### SYSTEM)', &
+      '  --stop S      comma-separated stop sequences', &
+      '  --stream T    stream tokens as generated (flush each)', &
       '  --stats T     print prefill/decode ms + tok/s to stderr'], &
-      version_text=[character(len=80) :: 'chat_text 1.0'])
+      version_text=[character(len=80) :: 'chat_text 1.1'])
   tdir = trim(sget('tables'))
   wdir = trim(sget('weights'))
   n_gen = iget('n')
@@ -105,6 +114,28 @@ program chat_text
   end do
   close (u)
 
+  ! --- templating (customizable, default = chat) ---
+  if (specified('template')) then
+    tmpl_raw = trim(sget('template'))
+  else
+    tmpl_raw = "### USER" // char(10) // "{prompt}" // char(10) // char(10) // "### ASSISTANT" // char(10)
+  end if
+  if (specified('system')) then
+    sys_raw = trim(sget('system'))
+  else
+    sys_raw = ""
+  end if
+  if (trim(tmpl_raw) /= "raw") then
+    block
+      integer, allocatable :: tmp(:)
+      integer :: ntmp
+      call apply_template(pbytes, fsize, tmpl_raw, sys_raw, tmp, ntmp)
+      deallocate(pbytes)
+      call move_alloc(tmp, pbytes)
+      fsize = ntmp
+    end block
+  end if
+
   call encode(pbytes, fsize, pids)
   nprompt = size(pids) + 1
   allocate(idx(nprompt + n_gen))
@@ -134,7 +165,9 @@ program chat_text
   ! needed is t=ntot-1 (its logits predict idx(ntot)); step ntot would
   ! write idx(ntot+1), out of bounds.
   dostats = specified('stats')
+  dostream = specified('stream')
   if (dostats) call system_clock(count_rate=srate)
+  if (dostream) open (newunit=u, file="/dev/stdout", access="stream", form="unformatted")
   cms_pre = 0; cms_dec = 0
   do step = 1, ntot - 1
     tc = step
@@ -159,17 +192,127 @@ program chat_text
     best = sample_next(out1, VV, temp, topp, pres, freq, &
         idx(nprompt+1:), tc - nprompt, nblock, rng)
     idx(tc+1) = best - 1
+    if (dostream) then
+      call decode(idx(tc+1:tc+1), 1, sbytes, snbytes)
+      do i = 1, snbytes
+        write (u) char(sbytes(i))
+      end do
+      flush(u)
+      deallocate(sbytes)
+    end if
   end do
 
   if (dostats) write (0, '(A,F10.1,A,F10.1,A,F8.2)') "stats: prefill_ms=", &
       1000.0 * real(cms_pre) / real(srate), " decode_ms=", &
       1000.0 * real(cms_dec) / real(srate), " tok_s=", &
       real(n_gen) / max(1.0e-9, real(cms_pre + cms_dec) / real(srate))
-  call decode(idx(nprompt+1:), n_gen, obytes, nbytes)
-  open (newunit=u, file="/dev/stdout", access="stream", form="unformatted")
-  do i = 1, nbytes
-    write (u) char(obytes(i))
-  end do
-  close (u)
+  if (dostream) then
+    close (u)
+  else
+    call decode(idx(nprompt+1:), n_gen, obytes, nbytes)
+    ! stop-sequence truncation (only for non-streaming; streaming already flushed)
+    if (specified('stop')) then
+      stop_raw = trim(sget('stop'))
+    else
+      stop_raw = "### USER,### SYSTEM"
+    end if
+    if (len_trim(stop_raw) > 0) call truncate_at_stop(obytes, nbytes, stop_raw)
+    open (newunit=u, file="/dev/stdout", access="stream", form="unformatted")
+    do i = 1, nbytes
+      write (u) char(obytes(i))
+    end do
+    close (u)
+  end if
+
+contains
+
+  subroutine apply_template(inp, n_in, tmpl, sys, out, n_out)
+    integer, intent(in) :: inp(*), n_in
+    character(*), intent(in) :: tmpl, sys
+    integer, allocatable, intent(out) :: out(:)
+    integer, intent(out) :: n_out
+    character(len=:), allocatable :: pre, post, sys_part, t2
+    integer :: at, k, n1, n2, ns, ii
+    allocate(character(len=len_trim(tmpl)) :: t2)
+    t2 = trim(tmpl)
+    call unescape_nl(t2)
+    if (len_trim(sys) > 0) then
+      sys_part = "### SYSTEM" // char(10) // trim(sys) // char(10) // char(10)
+    else
+      sys_part = ""
+    end if
+    at = index(t2, "{prompt}")
+    if (at > 0) then
+      pre = sys_part // t2(1:at-1)
+      post = t2(at+8:)
+    else
+      pre = sys_part // t2
+      post = ""
+    end if
+    n1 = len(pre); n2 = len(post); ns = len_trim(sys_part)
+    ! count without trimming inner spaces: use len of pre/post as built
+    n_out = len(pre) + n_in + len(post)
+    allocate(out(n_out))
+    k = 1
+    do ii = 1, len(pre)
+      out(k) = ichar(pre(ii:ii)); k = k + 1
+    end do
+    do ii = 1, n_in
+      out(k) = inp(ii); k = k + 1
+    end do
+    do ii = 1, len(post)
+      out(k) = ichar(post(ii:ii)); k = k + 1
+    end do
+  end subroutine apply_template
+
+  subroutine unescape_nl(s)
+    character(len=:), allocatable, intent(inout) :: s
+    character(len=:), allocatable :: r
+    integer :: i, j
+    allocate(character(len=len(s)) :: r)
+    j = 0
+    i = 1
+    do while (i <= len(s))
+      if (s(i:i) == "\\" .and. i < len(s) .and. s(i+1:i+1) == "n") then
+        j = j + 1; r(j:j) = char(10); i = i + 2
+      else if (s(i:i) == "\\" .and. i < len(s) .and. s(i+1:i+1) == "t") then
+        j = j + 1; r(j:j) = char(9); i = i + 2
+      else
+        j = j + 1; r(j:j) = s(i:i); i = i + 1
+      end if
+    end do
+    s = r(1:j)
+  end subroutine unescape_nl
+
+  subroutine truncate_at_stop(bytes, n, stops)
+    integer, intent(inout) :: n
+    integer, intent(in) :: bytes(*)
+    character(*), intent(in) :: stops
+    character(len=:), allocatable :: txt, stop1
+    integer :: p, q, cut, best_cut
+    allocate(character(len=n) :: txt)
+    do p = 1, n; txt(p:p) = char(bytes(p)); end do
+    best_cut = 0
+    p = 1
+    do
+      q = index(stops(p:), ",")
+      if (q == 0) then
+        stop1 = trim(adjustl(stops(p:)))
+        if (len_trim(stop1) > 0) then
+          cut = index(txt, trim(stop1))
+          if (cut > 0 .and. (best_cut == 0 .or. cut < best_cut)) best_cut = cut
+        end if
+        exit
+      else
+        stop1 = trim(adjustl(stops(p:p+q-2)))
+        if (len_trim(stop1) > 0) then
+          cut = index(txt, trim(stop1))
+          if (cut > 0 .and. (best_cut == 0 .or. cut < best_cut)) best_cut = cut
+        end if
+        p = p + q
+      end if
+    end do
+    if (best_cut > 0) n = best_cut - 1
+  end subroutine truncate_at_stop
 
 end program chat_text
--- src/app/repl.f90
+++ src/app/repl.f90
@@ -17,7 +17,7 @@ program repl
   use tokenizer_encode_mod
   use sample_mod, only: sample_token, sample_next
   use fortran_kv_mod, only: gpt_step
-  use M_CLI2, only: set_args, sget, rget, iget, specified
+  use M_CLI2, only: set_args, set_mode, sget, rget, iget, specified
   implicit none
 
   integer, parameter :: sp = c_float
@@ -27,16 +27,19 @@ program repl
 
   character(len=512) :: tdir, wdir, arg
   character(len=8192) :: linebuf
+  character(len=4096) :: tmpl_raw, sys_raw, stop_raw
   integer :: n_gen, u, ios, i, j, best, tc, step, nprompt, nbytes, nlen
   integer :: ntot, d2, clen
   logical :: dostats = .false.
+  logical :: dostream = .false.
   integer :: ca, cb, crate
   integer(c_int64_t) :: cms_pre = 0, cms_dec = 0
   real(sp), allocatable :: ckv(:), cvv(:), out1(:)
   integer(c_int64_t) :: rng = 12345_c_int64_t
   real(sp) :: temp = 0.0_sp, topp = 1.0_sp, pres = 0.0_sp, freq = 0.0_sp
   integer :: nblock = 0
-  integer, allocatable :: pbytes(:), pids(:), idx(:), obytes(:)
+  integer, allocatable :: pbytes(:), pids(:), idx(:), obytes(:), sbytes(:)
+  integer :: snbytes
   real(sp), allocatable :: cos_b(:), sin_b(:)
   real(sp), allocatable :: wte(:), lm(:)
   real(sp), allocatable :: c_q(:), c_k(:), c_v(:), c_pr(:), c_fc(:), c_pr2(:)
@@ -44,22 +47,28 @@ program repl
   real(sp) :: theta, ang, mchk
 
   n_gen = 40
+  call set_mode('response_file')
   call set_args('--tables TABLES --weights WEIGHTS --n 40 --temp 0.0' // &
       ' --seed 12345 --topp 1.0 --pres 0.0 --freq 0.0 --nblock 0' // &
-      ' --stats F', &
+      ' --stats F --template T --system S --stop S --stream F', &
       help_text=[character(len=80) :: &
       'NAME', &
       '  repl - interactive pure-Fortran chat REPL', &
       'SYNOPSIS', &
       '  repl --tables DIR --weights DIR [options]', &
+      '  repl @rsp_file (response file, see chat.rsp)', &
       'OPTIONS', &
       '  --tables DIR  tokenizer tables', &
       '  --weights DIR checkpoint .npy set', &
       '  --n N         tokens per turn (empty line quits)', &
       '  --temp T --seed S --topp P --pres X --freq X --nblock N', &
       '                sampling controls (see chat_text --help)', &
+      '  --template S  chat template with {prompt} (default: chat)', &
+      '  --system S    system prompt (prepended as ### SYSTEM)', &
+      '  --stop S      comma-separated stop sequences', &
+      '  --stream T    stream tokens as generated', &
       '  --stats T     print per-turn prefill/decode ms + tok/s'], &
-      version_text=[character(len=80) :: 'repl 1.0'])
+      version_text=[character(len=80) :: 'repl 1.1'])
   tdir = trim(sget('tables'))
   wdir = trim(sget('weights'))
   n_gen = iget('n')
@@ -91,6 +100,27 @@ program repl
     do i = 1, nlen
       pbytes(i) = ichar(linebuf(i:i))
     end do
+    ! templating (default = chat)
+    if (specified('template')) then
+      tmpl_raw = trim(sget('template'))
+    else
+      tmpl_raw = "### USER" // char(10) // "{prompt}" // char(10) // char(10) // "### ASSISTANT" // char(10)
+    end if
+    if (specified('system')) then
+      sys_raw = trim(sget('system'))
+    else
+      sys_raw = ""
+    end if
+    if (trim(tmpl_raw) /= "raw") then
+      block
+        integer, allocatable :: tmp(:)
+        integer :: ntmp
+        call apply_template(pbytes, nlen, tmpl_raw, sys_raw, tmp, ntmp)
+        deallocate(pbytes)
+        call move_alloc(tmp, pbytes)
+        nlen = ntmp
+      end block
+    end if
 
     call encode(pbytes, nlen, pids)
     deallocate(pbytes)
@@ -125,7 +155,9 @@ program repl
     allocate(out1(B*VV))
 
     dostats = specified('stats')
+    dostream = specified('stream')
     if (dostats) call system_clock(count_rate=crate)
+    if (dostream) open (newunit=u, file="/dev/stdout", access="stream", form="unformatted")
     cms_pre = 0; cms_dec = 0
     do step = 1, ntot - 1
       tc = step
@@ -151,6 +183,14 @@ program repl
       best = sample_next(out1, VV, temp, topp, pres, freq, &
           idx(nprompt+1:), tc - nprompt, nblock, rng)
       idx(tc+1) = best - 1
+      if (dostream) then
+        call decode(idx(tc+1:tc+1), 1, sbytes, snbytes)
+        do j = 1, snbytes
+          write (u) char(sbytes(j))
+        end do
+        flush(u)
+        deallocate(sbytes)
+      end if
     end do
     deallocate(cos_b, sin_b)
     ! ckv/cvv/out1 are deallocated together at top of next turn
@@ -161,15 +201,114 @@ program repl
         1000.0 * real(cms_pre) / real(crate), " decode_ms=", &
         1000.0 * real(cms_dec) / real(crate), " tok_s=", &
         real(n_gen) / max(1.0e-9, real(cms_pre + cms_dec) / real(crate))
-    call decode(idx(nprompt+1:), n_gen, obytes, nbytes)
-    open (newunit=u, file="/dev/stdout", access="stream", form="unformatted")
-    do i = 1, nbytes
-      write (u) char(obytes(i))
-    end do
-    write (u) char(10)
-    close (u)
-    deallocate(idx, obytes)
+    if (dostream) then
+      write (u) char(10)
+      close (u)
+      deallocate(idx)
+    else
+      call decode(idx(nprompt+1:), n_gen, obytes, nbytes)
+      if (specified('stop')) then
+        stop_raw = trim(sget('stop'))
+      else
+        stop_raw = "### USER,### SYSTEM"
+      end if
+      if (len_trim(stop_raw) > 0) call truncate_at_stop(obytes, nbytes, stop_raw)
+      open (newunit=u, file="/dev/stdout", access="stream", form="unformatted")
+      do i = 1, nbytes
+        write (u) char(obytes(i))
+      end do
+      write (u) char(10)
+      close (u)
+      deallocate(idx, obytes)
+    end if
   end do
   write (0, '(A)') "bye."
+
+contains
+
+  subroutine apply_template(inp, n_in, tmpl, sys, out, n_out)
+    integer, intent(in) :: inp(*), n_in
+    character(*), intent(in) :: tmpl, sys
+    integer, allocatable, intent(out) :: out(:)
+    integer, intent(out) :: n_out
+    character(len=:), allocatable :: pre, post, sys_part, t2
+    integer :: at, k, ii
+    allocate(character(len=len_trim(tmpl)) :: t2)
+    t2 = trim(tmpl)
+    call unescape_nl(t2)
+    if (len_trim(sys) > 0) then
+      sys_part = "### SYSTEM" // char(10) // trim(sys) // char(10) // char(10)
+    else
+      sys_part = ""
+    end if
+    at = index(t2, "{prompt}")
+    if (at > 0) then
+      pre = sys_part // t2(1:at-1)
+      post = t2(at+8:)
+    else
+      pre = sys_part // t2
+      post = ""
+    end if
+    n_out = len(pre) + n_in + len(post)
+    allocate(out(n_out))
+    k = 1
+    do ii = 1, len(pre)
+      out(k) = ichar(pre(ii:ii)); k = k + 1
+    end do
+    do ii = 1, n_in
+      out(k) = inp(ii); k = k + 1
+    end do
+    do ii = 1, len(post)
+      out(k) = ichar(post(ii:ii)); k = k + 1
+    end do
+  end subroutine apply_template
+
+  subroutine unescape_nl(s)
+    character(len=:), allocatable, intent(inout) :: s
+    character(len=:), allocatable :: r
+    integer :: i, j
+    allocate(character(len=len(s)) :: r)
+    j = 0; i = 1
+    do while (i <= len(s))
+      if (s(i:i) == "\\" .and. i < len(s) .and. s(i+1:i+1) == "n") then
+        j = j + 1; r(j:j) = char(10); i = i + 2
+      else if (s(i:i) == "\\" .and. i < len(s) .and. s(i+1:i+1) == "t") then
+        j = j + 1; r(j:j) = char(9); i = i + 2
+      else
+        j = j + 1; r(j:j) = s(i:i); i = i + 1
+      end if
+    end do
+    s = r(1:j)
+  end subroutine unescape_nl
+
+  subroutine truncate_at_stop(bytes, n, stops)
+    integer, intent(inout) :: n
+    integer, intent(in) :: bytes(*)
+    character(*), intent(in) :: stops
+    character(len=:), allocatable :: txt, stop1
+    integer :: p, q, cut, best_cut
+    allocate(character(len=n) :: txt)
+    do p = 1, n; txt(p:p) = char(bytes(p)); end do
+    best_cut = 0; p = 1
+    do
+      q = index(stops(p:), ",")
+      if (q == 0) then
+        stop1 = trim(adjustl(stops(p:)))
+        if (len_trim(stop1) > 0) then
+          cut = index(txt, trim(stop1))
+          if (cut > 0 .and. (best_cut == 0 .or. cut < best_cut)) best_cut = cut
+        end if
+        exit
+      else
+        stop1 = trim(adjustl(stops(p:p+q-2)))
+        if (len_trim(stop1) > 0) then
+          cut = index(txt, trim(stop1))
+          if (cut > 0 .and. (best_cut == 0 .or. cut < best_cut)) best_cut = cut
+        end if
+        p = p + q
+      end if
+    end do
+    if (best_cut > 0) n = best_cut - 1
+  end subroutine truncate_at_stop
 
 end program repl
